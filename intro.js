e4ca3a2a408a3939cc9066f1a61b6e3652bf434c
Nicholas <nicholaschikangira7@gmail.com>
2026-05-14 11:07:28 +0200

    task1

---
+++ intro.js
@@ -0,0 +1,7 @@
+const name = 'Nicholas';
+let age = 20;
+let programmingLanguage = 'javascript';
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Programming Language: ${programmingLanguage}`);
